502eabb5537098210f909470a58c44eca3ea7825
Tapish <narwal83@hzdr.de>
2024-05-16 10:49:26 +0200

    Add example which fails if task finishes before init graph is called

---
+++ examples/8_child_destruction.cpp
@@ -0,0 +1,52 @@
+/* Copyright 2024 Tapish Narwal
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_OFF
+
+#include <redGrapes/redGrapes.hpp>
+#include <redGrapes/resource/ioresource.hpp>
+
+#include <iostream>
+
+int main()
+{
+    spdlog::set_level(spdlog::level::off);
+
+    auto rg = redGrapes::init(1);
+    auto a = rg.createIOResource<int>(1);
+
+    rg.emplace_task(
+          [&rg]([[maybe_unused]] auto a)
+          {
+              std::cout << "scope = " << rg.scope_depth() << " a = " << *a << std::endl;
+
+              rg.emplace_task(
+                  [&rg](auto a)
+                  {
+                      *a = 2;
+                      std::cout << "scope = " << rg.scope_depth() << " a = " << *a << std::endl;
+                  },
+                  a.write());
+              rg.emplace_task(
+                  [&rg](auto a)
+                  {
+                      *a = 3;
+                      std::cout << "scope = " << rg.scope_depth() << " a = " << *a << std::endl;
+                  },
+                  a.write());
+
+              *a = 4;
+              std::cout << "scope = " << rg.scope_depth() << " a = " << *a << std::endl;
+          },
+          a.write())
+        .enable_stack_switching();
+    rg.emplace_task(
+          [&rg]([[maybe_unused]] auto a)
+          { std::cout << "scope = " << rg.scope_depth() << " a = " << *a << std::endl; },
+          a.read())
+        .enable_stack_switching();
+}
